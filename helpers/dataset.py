4cf4ab7f2992b93265b80ec53e313040046b9f15
Dennis Shushack <dennis.shushack@ost.ch>
2024-05-06 11:23:35 +0000

    addidng required pre-processing steps

--- helpers/dataset.py
+++ helpers/dataset.py
@@ -1,7 +1,8 @@
-import tensorflow as tf
+from pathlib import Path
+
 import numpy as np
 import rasterio
-from pathlib import Path
+import tensorflow as tf
 
 
 class ImageDataLoader:
@@ -10,8 +11,46 @@ class ImageDataLoader:
     - Loads and processes the images and masks
     - Applies data augmentation: Flipping, Rotating and adding speckle noise
     """
+
     def __init__(self, data_path):
         self.data_path = Path(data_path)
         self.base_path = self.data_path.parent
+        self.satellite_dir = self.base_path / 'satellite'
+        self.mask_dir = self.base_path / 'mask'
+
+    def load_and_preprocess_image(self, image_path):
+        """
+        Loads the satellite image into the tensorflow dataset and
+        transforms it into the correct format (height, width, channels)
+        """
+
+        def _load_image(path):
+            with rasterio.open(path.decode('utf-8')) as src:
+                image = src.read().transpose((1, 2, 0))
+                return image.astype(np.float32)
+
+        tensor = tf.numpy_function(_load_image, [image_path], tf.float32)
+        tensor.ensure_shape((1024, 1024, 4))
+        return tensor
+
+    def create_dataset(self):
+        image_paths = sorted(self.satellite_dir.glob('*.tif'),
+                             key=lambda x: int(x.stem.split('_')[-1]))
+        mask_paths = sorted(self.mask_dir.glob('*.tif'),
+                            key=lambda x: int(x.stem.replace('_mask', '').split('_')[-1]))
+
+        # Transform into string tensors
+        image_paths = tf.constant([str(path) for path in image_paths])
+        mask_paths = tf.constant([str(path) for path in mask_paths])
+        assert len(image_paths) == len(mask_paths)
+
+        # Print available gpus
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        print(gpus)
+        dataset = tf.data.Dataset.from_tensor_slices((image_paths, mask_paths))
 
 
+if __name__ == '__main__':
+    data_path = '/project/Satelite/data/AG.gpkg'
+    loader = ImageDataLoader(data_path)
+    loader.create_dataset()
