97b1ad66b03ac09dd46d6c6297186aeb777666f4
Dennis Shushack <dennis.shushack@ost.ch>
2024-05-13 13:45:28 +0000

    Added the tensorflow dataset creation script

--- helpers/dataset.py
+++ helpers/dataset.py
@@ -9,12 +9,11 @@ import numpy as np
 import rasterio
 
 
-
-class ImageDataLoader:
+class CreateTensorflowDataset:
     """
     This class prepares the tensorflow dataset for training:
-    - Loads and processes the images and masks
-    - Applies data augmentation: Flipping, Rotating, Adds noise, gaussian blur and zooming in.
+    - Loads and processes the images and masks in order to be suiting for 
+    a tensorflow dataset.
     """
 
     def __init__(self, data_path):
@@ -56,96 +55,30 @@ class ImageDataLoader:
         tensor = tf.numpy_function(_load_mask, [mask_path], tf.uint8)
         tensor.set_shape([1024, 1024, 1])
         return tensor
-
-    def add_speckle_noise(self, image):
-        """Apply speckle noise to an already normalized image."""
-        speckle_variance = np.random.uniform(0.01, 0.1)
-        noise = tf.random.normal(shape=tf.shape(image), mean=1.0, stddev=tf.sqrt(speckle_variance))
-        noisy_image = image * noise
-        return noisy_image
-
-    def add_gaussian_noise(self, image):
-        """Apply gaussian noise to an already normalized image."""
-        noise_variance = np.random.uniform(0.01, 0.1)
-        noise = tf.random.normal(shape=tf.shape(image), mean=0.0, stddev=tf.sqrt(noise_variance))
-        noisy_image = image + noise
-        return noisy_image
-
-    def add_salt_and_pepper_noise(self, image):
-        """
-        Adds salt and pepper noise to an already normalized image.
-        """
-        noise_level = np.random.uniform(0.01, 0.1)
-        shape = tf.shape(image)
-        # Creates a tensors with the same size as the image with random values between 0 and 1:
-        random_tensor = tf.random.uniform(shape, minval=0, maxval=1.0)
-
-        # Creates a salt mask (white pixels) i.e. at 6% noise_level => 3% of the pixels will be white
-        # Creates a mask with 0 and 1 values where 1 is the pixels that will be white
-        salt_mask = tf.cast(random_tensor <= (noise_level / 2), image.dtype)
-        # Creates a pepper mask (black pixels) i.e. at 6% noise_level => 3% of the pixels will be black
-        # Creates a mask with 0 and 1 values where 1 is the pixels that will be black
-        pepper_mask = tf.cast(random_tensor >= (1 - noise_level / 2), image.dtype)
-        # Apply the masks to the image
-        image_with_salt = tf.where(salt_mask == 1, tf.ones_like(image), image)
-        noisy_image = tf.where(pepper_mask == 1, tf.zeros_like(image), image_with_salt)
-
-        return noisy_image
-
-    def add_gaussian_blur(self, image):
-        """
-        Apply gaussian blur to an already normalized image using scipy. 
-        The function expects a tensorflow tensor and returns a blurred version of the image. 
-        Each band is blurred independently to maintain spectral integrity.
-        """
-        def _apply_blur(image):
-            # Define the standard deviation for the Gaussian Kernel: (Higher values will increase the blur)
-            sigma = np.random.uniform(0.5, 1.5)
-            blurred_image = np.zeros_like(image)
-            for i in range(4):
-                blurred_image[:, :, i] = gaussian_filter(image[:, :, i], sigma=sigma)
-            return blurred_image.astype(np.float32)
+    
+    def prepare_dataset(self):
+        print("Preparing the tensorflow dataset...")
+        image_paths = sorted(self.satellite_dir.glob('*.tif'), key=lambda x: int(x.stem.split('_')[-1]))
+        mask_paths = sorted(self.mask_dir.glob('*.tif'), key=lambda x: int(x.stem.replace('_mask', '').split('_')[-1]))
+        image_paths = [str(path) for path in image_paths]
+        mask_paths = [str(path) for path in mask_paths]
         
-        blurred_image = tf.numpy_function(_apply_blur, [image], tf.float32)
-        blurred_image.set_shape([1024, 1024, 4])
-        return blurred_image
-
-    def add_rotation(self, image, mask):
-        """
-        Rotate image by +- 90 degrees
-        """
-        rotation = [+1, -1]
-        random_rotation = np.random.choice(rotation)
-        image = tf.image.rot90(image, k=random_rotation)
-        mask = tf.image.rot90(mask, k=random_rotation)
-        return image, mask
-
-    def add_horizontal_flip(self, image, mask):
-        """
-        Flip image horizontally
-        """
-        image = tf.image.flip_left_right(image)
-        mask = tf.image.flip_left_right(mask)
-        return image, mask
-
-    def create_dataset(self):
-        image_paths = sorted(self.satellite_dir.glob('*.tif'),
-                             key=lambda x: int(x.stem.split('_')[-1]))
-        mask_paths = sorted(self.mask_dir.glob('*.tif'),
-                            key=lambda x: int(x.stem.replace('_mask', '').split('_')[-1]))
-
-        # Transform into string tensors
-        image_paths = tf.constant([str(path) for path in image_paths])
-        mask_paths = tf.constant([str(path) for path in mask_paths])
-        assert len(image_paths) == len(mask_paths)
-
-        # Print available gpus
-        gpus = tf.config.experimental.list_physical_devices('GPU')
-        print(gpus)
         dataset = tf.data.Dataset.from_tensor_slices((image_paths, mask_paths))
-
+        dataset = dataset.map(lambda x, y: (self.process_image(x), self.process_mask(y)),
+                              num_parallel_calls=tf.data.AUTOTUNE)
+        
+        # Save the tensorflow dataset:
+        save_path = self.base_path / 'dataset'
+        if not save_path.exists():
+            save_path.mkdir()
+            
+        tf.data.Dataset.save(
+            dataset, str(save_path), compression=None, shard_func=None, checkpoint_args=None
+        )
+        print("Tensorflow dataset saved at: ", save_path)
+      
 
 if __name__ == '__main__':
-    data_path = '/project/Satelite/data/AG.gpkg'
-    loader = ImageDataLoader(data_path)
-    loader.create_dataset()
+    data_path = '/home/tfuser/project/Satelite/data'
+    loader = CreateTensorflowDataset(data_path)
+    loader.prepare_dataset()
