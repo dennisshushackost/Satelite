5eb18da3d9261e0ce6313009eee1a387ef67f5d9
Dennis Shushack <dennis.shushack@ost.ch>
2024-06-13 22:33:14 +0200

    s
    s

--- models/model.py
+++ models/model.py
@@ -348,6 +348,96 @@ def Attention_ResUNet(input_shape, NUM_CLASSES=1, dropout_rate=0.0, batch_norm=T
     print(model.summary())
     return model
 
+def Deep_Attention_RESUNET(input_shape, NUM_CLASSES=1, dropout_rate=0.0, batch_norm=True):
+    '''
+    Residual UNet, with attention 
+    
+    '''
+    # network structure
+    FILTER_NUM = 64 # number of basic filters for the first layer
+    FILTER_SIZE = 3 # size of the convolutional filter
+    UP_SAMP_SIZE = 2 # size of upsampling filters
+    # input data
+    # dimension of the image depth
+    inputs = layers.Input(input_shape, dtype=tf.float32)
+    axis = 3
+
+    # Downsampling layers
+    # DownRes 1, double residual convolution + pooling
+    conv_256 = res_conv_block(inputs, FILTER_SIZE, FILTER_NUM, dropout_rate, batch_norm)
+    pool_128 = layers.MaxPooling2D(pool_size=(2,2))(conv_256)
+    # DownRes 2
+    conv_128 = res_conv_block(pool_128, FILTER_SIZE, 2*FILTER_NUM, dropout_rate, batch_norm)
+    pool_64 = layers.MaxPooling2D(pool_size=(2,2))(conv_128)
+    # DownRes 3
+    conv_64 = res_conv_block(pool_64, FILTER_SIZE, 4*FILTER_NUM, dropout_rate, batch_norm)
+    pool_32 = layers.MaxPooling2D(pool_size=(2,2))(conv_64)
+    # DownRes 4
+    conv_32 = res_conv_block(pool_32, FILTER_SIZE, 8*FILTER_NUM, dropout_rate, batch_norm)
+    pool_16 = layers.MaxPooling2D(pool_size=(2,2))(conv_32)
+    # DownRes 5
+    conv_16 = res_conv_block(pool_16, FILTER_SIZE, 16*FILTER_NUM, dropout_rate, batch_norm)
+    pool_8 = layers.MaxPooling2D(pool_size=(2,2))(conv_16)
+    # DownRes 6
+    conv_8 = res_conv_block(pool_8, FILTER_SIZE, 32*FILTER_NUM, dropout_rate, batch_norm)
+    pool_4 = layers.MaxPooling2D(pool_size=(2,2))(conv_8)
+    # DownRes 7, convolution only
+    conv_4 = res_conv_block(pool_4, FILTER_SIZE, 64*FILTER_NUM, dropout_rate, batch_norm)
+
+    # Upsampling layers
+    # UpRes 8, attention gated concatenation + upsampling + double residual convolution
+    gating_8 = gating_signal(conv_4, 32*FILTER_NUM, batch_norm)
+    att_8 = attention_block(conv_8, gating_8, 32*FILTER_NUM)
+    up_8 = layers.UpSampling2D(size=(UP_SAMP_SIZE, UP_SAMP_SIZE), data_format="channels_last")(conv_4)
+    up_8 = layers.concatenate([up_8, att_8], axis=axis)
+    up_conv_8 = res_conv_block(up_8, FILTER_SIZE, 32*FILTER_NUM, dropout_rate, batch_norm)
+    # UpRes 9
+    gating_16 = gating_signal(up_conv_8, 16*FILTER_NUM, batch_norm)
+    att_16 = attention_block(conv_16, gating_16, 16*FILTER_NUM)
+    up_16 = layers.UpSampling2D(size=(UP_SAMP_SIZE, UP_SAMP_SIZE), data_format="channels_last")(up_conv_8)
+    up_16 = layers.concatenate([up_16, att_16], axis=axis)
+    up_conv_16 = res_conv_block(up_16, FILTER_SIZE, 16*FILTER_NUM, dropout_rate, batch_norm)
+    # UpRes 10
+    gating_32 = gating_signal(up_conv_16, 8*FILTER_NUM, batch_norm)
+    att_32 = attention_block(conv_32, gating_32, 8*FILTER_NUM)
+    up_32 = layers.UpSampling2D(size=(UP_SAMP_SIZE, UP_SAMP_SIZE), data_format="channels_last")(up_conv_16)
+    up_32 = layers.concatenate([up_32, att_32], axis=axis)
+    up_conv_32 = res_conv_block(up_32, FILTER_SIZE, 8*FILTER_NUM, dropout_rate, batch_norm)
+    # UpRes 11
+    gating_64 = gating_signal(up_conv_32, 4*FILTER_NUM, batch_norm)
+    att_64 = attention_block(conv_64, gating_64, 4*FILTER_NUM)
+    up_64 = layers.UpSampling2D(size=(UP_SAMP_SIZE, UP_SAMP_SIZE), data_format="channels_last")(up_conv_32)
+    up_64 = layers.concatenate([up_64, att_64], axis=axis)
+    up_conv_64 = res_conv_block(up_64, FILTER_SIZE, 4*FILTER_NUM, dropout_rate, batch_norm)
+    # UpRes 12
+    gating_128 = gating_signal(up_conv_64, 2*FILTER_NUM, batch_norm)
+    att_128 = attention_block(conv_128, gating_128, 2*FILTER_NUM)
+    up_128 = layers.UpSampling2D(size=(UP_SAMP_SIZE, UP_SAMP_SIZE), data_format="channels_last")(up_conv_64)
+    up_128 = layers.concatenate([up_128, att_128], axis=axis)
+    up_conv_128 = res_conv_block(up_128, FILTER_SIZE, 2*FILTER_NUM, dropout_rate, batch_norm)
+    # UpRes 13
+    gating_256 = gating_signal(up_conv_128, FILTER_NUM, batch_norm)
+    att_256 = attention_block(conv_256, gating_256, FILTER_NUM)
+    up_256 = layers.UpSampling2D(size=(UP_SAMP_SIZE, UP_SAMP_SIZE), data_format="channels_last")(up_conv_128)
+    up_256 = layers.concatenate([up_256, att_256], axis=axis)
+    up_conv_256 = res_conv_block(up_256, FILTER_SIZE, FILTER_NUM, dropout_rate, batch_norm)
+    # UpRes 14
+    up_512 = layers.UpSampling2D(size=(UP_SAMP_SIZE, UP_SAMP_SIZE), data_format="channels_last")(up_conv_256)
+    up_conv_512 = res_conv_block(up_512, FILTER_SIZE, FILTER_NUM, dropout_rate, batch_norm)
+
+    # 1*1 convolutional layers
+    conv_final = layers.Conv2D(NUM_CLASSES, kernel_size=(1,1))(up_conv_512)
+    conv_final = layers.BatchNormalization(axis=axis)(conv_final)
+    conv_final = layers.Activation('sigmoid')(conv_final)  #Change to softmax for multichannel
+
+    # Model integration
+    model = models.Model(inputs, conv_final, name="AttentionResUNet")
+    print(model.summary())
+    return model
+    
+    
+    
+
 def DeepUnet(input_shape, NUM_CLASSES=1, dropout_rate=0.0, batch_norm=True):
     '''
     UNet, 
@@ -409,5 +499,5 @@ def DeepUnet(input_shape, NUM_CLASSES=1, dropout_rate=0.0, batch_norm=True):
     print(model.summary())
     return model
 
-model = Attention_ResUNet((256, 256, 4), NUM_CLASSES=1, dropout_rate=0.0, batch_norm=True)
+model = Deep_Attention_RESUNET((256, 256, 4), NUM_CLASSES=1, dropout_rate=0.0, batch_norm=True)
 model.summary()
